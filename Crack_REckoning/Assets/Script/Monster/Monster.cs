c10443abe3b4078287a12b5e2af1a37ef5f67fc9
김의중 <htp31698@naver.com>
2025-09-12 15:26:53 +0900

    Monster Move & Attack

--- Crack_REckoning/Assets/Script/Monster/Monster.cs
+++ Crack_REckoning/Assets/Script/Monster/Monster.cs
@@ -3,19 +3,42 @@ using UnityEngine.AI;
 
 public class Monster : MonoBehaviour
 {
-    [SerializeField] Transform target;
+    [SerializeField]
+    public Transform target;
+    private Animator animator;
 
-    NavMeshAgent agent;
+    private NavMeshAgent agent;
+    private float posX;
+    private float posZ;
+    private bool isAttackAble;
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+        posX = gameObject.transform.position.x;
+        posZ = gameObject.transform.position.z;
     }
 
     private void Update()
     {
-        agent.SetDestination(target.position);
+        //Target Move
+        Vector3 movepos = new Vector3(posX, target.position.y, posZ);
+        agent.SetDestination(movepos);
+
+        //Target Attack
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                {
+                    animator.SetTrigger("Attack");
+                }
+            }
+        }
     }
 }
