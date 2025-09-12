60bba7aeb50f15ad47603ffe746d93273fab09c6
김의중 <htp31698@naver.com>
2025-09-12 18:06:32 +0900

    add Weakness

--- Crack_REckoning/Assets/Script/Monster/MonsterData.cs
+++ Crack_REckoning/Assets/Script/Monster/MonsterData.cs
@@ -1,25 +1,30 @@
-using Unity.VisualScripting;
 using UnityEngine;
-
-[CreateAssetMenu(fileName = "MonsterData", menuName = "Scriptable Objects/MonsterData")]
-public class MonsterData : ScriptableObject
+enum MonseterWeakness
 {
-    public int MonsterID;
+    Fire,
+    Water,
+    Lightning,
+}
 
-    public int MonsterName;
-    public float MonsterRange;
-    public int MonsterHp;
-    public int MonsterAttack;
-    public float MonsterSpeed;
-    public float MonsterAttackSpeed;
-    public int MonsterWeakness;
-    public int MonsterStrength;
+public class MonsterData
+{
+    public string MonsterID { get; set; }
+    public int MonsterName { get; set; }
+    public float MonsterRange { get; set; }
+    public int MonsterHp { get; set; }
+    public int MonsterAttack { get; set; }
+    public float MonsterSpeed { get; set; }
+    public float MonsterAttackSpeed { get; set; }
+    public int MonsterWeakness { get; set; }
+    public int MonsterStrength { get; set; }
 
-    public int MonsterEffectID;
+    public string MonsterEffectID { get; set; }
 
-    public int MonsterExp;
-    public string MonsterAnimator;
+    public int MonsterExp { get; set; }
+    public string MonsterDec { get; set; }
 
-
-    
+    public Sprite sprite
+        => Resources.Load<Sprite>($"Sprite/{MonsterName}");
+    public RuntimeAnimatorController AnimatorController
+        => Resources.Load<RuntimeAnimatorController>($"Animator/{MonsterName}");
 }
