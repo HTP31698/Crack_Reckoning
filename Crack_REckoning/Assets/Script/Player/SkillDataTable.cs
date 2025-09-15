09a6256dcab01ff41e1ac606328e3656a6cac169
김의중 <htp31698@naver.com>
2025-09-15 17:18:24 +0900

    skill setting

---
+++ Crack_REckoning/Assets/Script/Player/SkillDataTable.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class SkillDataTable : DataTable
+{
+    public override void Load(string filename)
+    {
+        throw new System.NotImplementedException();
+    }
+}
