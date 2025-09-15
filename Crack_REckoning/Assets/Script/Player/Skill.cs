dfc10819f672bb0d805c8af5e8a3b941d2651c1c
김의중 <htp31698@naver.com>
2025-09-15 17:53:56 +0900

    object setting

--- Crack_REckoning/Assets/Script/Player/Skill.cs
+++ Crack_REckoning/Assets/Script/Player/Skill.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 
 public class Skill : MonoBehaviour
 {
+    public GameObject basicSkill;
+    public GameObject skill1;
+    public GameObject skill2;
+    public GameObject skill3;
+    public GameObject skill4;
+
 
 }
--- Crack_REckoning/Assets/Script/Player/SkillSelectionDataTable.cs
+++ Crack_REckoning/Assets/Script/Player/SkillSelectionDataTable.cs
@@ -1,10 +1,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SkillDataTable : DataTable
+public class SkillSelectionDataTable : DataTable
 {
-    Dictionary<int, SkillData> table = new Dictionary<int, SkillData>();
-
+    Dictionary<(int, int), SkillSelectionData> table
+        = new Dictionary<(int, int), SkillSelectionData>();
     public override void Load(string filename)
     {
         throw new System.NotImplementedException();
